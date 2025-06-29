1116490217bbc12f0b01424f5f26a4ee1ce6c499
rikihardianto2 <amamiyarenncoc@gmail.com>
2025-06-29 08:17:29 +0700

    Fix: Mengubah sintaks tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './content/**/*.mdx',
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      ...colors,
+    },
+  },
+  plugins: [],
+}
+ 
